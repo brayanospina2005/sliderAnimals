a7ee6140f5eb9c27a5c823387abe0c1991d732f1
brayanospina2005 <ospinabrayan431@gmail.com>
2024-05-26 23:35:19 -0500

    feat: Update carousel styles and timing in CSS and JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,8 +9,8 @@ let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
 let timeDom = document.querySelector('.carousel .time');
 
 thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-let timeRunning = 3000;
-let timeAutoNext = 7000;
+let timeRunning = 2000;
+let timeAutoNext = 12000;
 
 nextDom.onclick = function(){
     showSlider('next');    
